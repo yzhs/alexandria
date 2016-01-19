a79e3528584d23910c0ef3968d57e3b8b4f24737
Colin Benner <colin@yzhs.de>
2016-01-19 21:57:27 +0100

    Automatically trim images

--- render/xelatex/Render.go
+++ render/xelatex/Render.go
@@ -45,9 +45,10 @@ func latexToPdf(id Id) error {
 
 func pdfToPng(i Id) error {
 	id := string(i)
-	return exec.Command("convert", "-quality", strconv.Itoa(Config.Quality),
-		"-density", strconv.Itoa(Config.Dpi), Config.TempDirectory+id+".pdf",
-		Config.CacheDirectory+id+".png").Run()
+	return exec.Command("convert", "-trim",
+		"-quality", strconv.Itoa(Config.Quality),
+		"-density", strconv.Itoa(Config.Dpi),
+		Config.TempDirectory+id+".pdf", Config.CacheDirectory+id+".png").Run()
 }
 
 func searchSwish(query []string) ([]Id, error) {
