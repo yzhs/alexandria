2e837dd457b2876b304a24c8ae0930f93cdb4d32
Colin Benner <colin@yzhs.de>
2018-10-17 15:06:37 +0200

    Simplify license header

--- lib.go
+++ lib.go
@@ -1,19 +1,6 @@
-// Alexandria
-//
-// Copyright (C) 2015-2018  Colin Benner
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// This file is part of Alexandria which is released under AGPLv3.
+// See LICENSE or go to https://github.com/yzhs/alexandria/LICENSE for full
+// license details.
 
 package alexandria
 
